302f39fc952aebec63d237bf4f2db2a91976c357
Manvendra Kumar <mopshyai@gmail.com>
2026-09-07 01:32:14 -0400

    test(mobile): align env mock with canonical API base

--- apps/mobile/__mocks__/@env.js
+++ apps/mobile/__mocks__/@env.js
@@ -6,10 +6,10 @@
 module.exports = {
   APP_ENV: 'test',
   APP_NAME: 'CareBow',
-  API_BASE_URL: 'https://api.carebow.com/v1',
+  API_BASE_URL: 'https://www.carebow.com/api',
   API_TIMEOUT: '30000',
-  ASK_CAREBOW_API_URL: 'https://api.carebow.com/v1/ask-carebow',
-  ASK_CAREBOW_API_KEY: 'test_api_key',
+  ANALYTICS_KEY: '',
+  SENTRY_DSN: '',
   FEATURE_HEALTH_MEMORY: 'true',
   FEATURE_IMAGE_UPLOAD: 'true',
   FEATURE_VOICE_INPUT: 'true',
